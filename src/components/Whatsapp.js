8f0ebb50441428a873986e52b76147dc4f37f3ac
Alberto <elbeto182@hotmail.com>
2021-03-13 01:06:34 -0300

    fix on whatsapp number, added sliderbar on mobile

--- src/components/Whatsapp.js
+++ src/components/Whatsapp.js
@@ -3,7 +3,7 @@ import icons from '../img/sprite.svg';
 
 export default function Whatsapp() {
   return (
-    <a className="whatsapp" target="_blank" rel="noreferrer" href={`https://api.whatsapp.com/send?phone=${process.env.REACT_APP_KIMI_PHONE}`}>
+    <a className="whatsapp" target="_blank" rel="noreferrer" href={`https://api.whatsapp.com/send?phone=541173679175`}>
       <svg className="whatsapp-icon">        
         <use xlinkHref={`${icons}#icon-whatsapp`}></use>
       </svg>
