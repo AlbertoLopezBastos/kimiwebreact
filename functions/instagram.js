7fe6db88475119989d626016408747fd2f570bcc
Alberto <elbeto182@hotmail.com>
2021-03-13 18:52:53 -0300

    fix on cache

--- functions/instagram.js
+++ functions/instagram.js
@@ -1,6 +1,6 @@
 require('isomorphic-fetch');
 
-const url = 'https://www.instagram.com/graphql/query/?query_hash=42323d64886122307be10013ad2dcc44&variables={%22id%22:10879409002,%22first%22:9}';
+const url = 'https://www.instagram.com/graphql/query/?query_hash=142323d64886122307be10013ad2dcc44&variables={%22id%22:10879409002,%22first%22:9}';
 
 const cache = {
   lastFetch: 0,
@@ -13,27 +13,37 @@ async function getPosts(){
   console.log('cache on init', cache.posts)
   const timeSinceLastFetch = Date.now() - cache.lastFetch;
   if(timeSinceLastFetch <= 1800000){
+    console.log('will return cache')
     return cache.posts;
   }
-
+  console.log('cache is old, will fetch new posts')
   const data = await fetch(url)
-                    .then(res => res.json())
+                    .then(res => {
+                      console.log('new posts fetched')
+                      return res.json()
+                    })
                     .catch(err => { 
                       console.log('error on fetch', err)
                       return [];
                     });
-  if(data == []){
-    return data;
+  console.log('variable with posts fetched', data)
+  if(data === [] || data.status === 'fail'){
+    console.log('data is [] will return that')
+    return [];
   }  
 
   const posts = slimUpPosts(data);
+  console.log('posts slimed', posts)
+  console.log('will create new cache');
   cache.lastFetch = Date.now();
   cache.posts = posts;
+  console.log('new cache created', cache)
   return posts;
 }
 
 function slimUpPosts(response){
   if(response.data){
+    console.log('slimpUpPosts, will slim ', response.data)
     return response.data.user.edge_owner_to_timeline_media.edges.map(edge=> ({
       thumbnail: edge.node.thumbnail_src,
       url: `https://instagram.com/p/${edge.node.shortcode}`,
@@ -47,7 +57,7 @@ function slimUpPosts(response){
 exports.handler = async function (event, context, callback) {
   const posts = await getPosts();
   console.log('final result', posts);
-  console.log('final cache', posts);
+  console.log('final cache', cache);
   callback(null, {
     statusCode: 200,
     headers: {
