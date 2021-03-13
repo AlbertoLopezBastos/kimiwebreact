ae0ffe6e9bb01131c681865617b2ea6b5f8cf8cf
Alberto <elbeto182@hotmail.com>
2021-03-13 00:55:50 -0300

    finished menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,33 +1,34 @@
-import React from 'react';
+import React, {useRef}from 'react';
 import ReactDom from 'react-dom';
 
 export default function Menu() {
 
-  const handleCloseMenu= () => {
-    return 'hi';
-  }
+const check = useRef();
 
+  const handleCloseMenu = () => {
+    check.current.checked = false;
+  }
 
   return ReactDom.createPortal(
     <div className="menu">
-      <div className="navigation">
-          <input type="checkbox" id="navi-toggle" className="navigation__checkbox"/>
-          <label htmlFor="navi-toggle" className="navigation__button">
-              <span className="navigation__icon">&nbsp;</span>
+      <div className="menu">
+          <input type="checkbox" ref={check} id="navi-toggle" className="menu__checkbox"/>
+          <label htmlFor="navi-toggle" className="menu__button">
+              <span className="menu__icon">&nbsp;</span>
           </label>
-          <div className="navigation__background">&nbsp;</div>
+          <div className="menu__background">&nbsp;</div>
 
-          <nav className="navigation__nav">
-              <ul className="navigation__list">
-                  <li onClick={() => { handleCloseMenu()}} className="navigation__item"><a href="#agendas" className="navigation__link">Agendas</a></li>
-                  <li onClick={() => { handleCloseMenu()}} className="navigation__item"><a href="#cuadernos" className="navigation__link">Cuadernos</a></li>
-                  <li onClick={() => { handleCloseMenu()}} className="navigation__item"><a href="#kakeibo" className="navigation__link">Kakeibo</a></li>
-                  <li onClick={() => { handleCloseMenu()}} className="navigation__item"><a href="#babys" className="navigation__link">Babys</a></li>
-                  <li onClick={() => { handleCloseMenu()}} className="navigation__item"><a href="#mascotas" className="navigation__link">Mascotas</a></li>
-                  <li onClick={() => { handleCloseMenu()}} className="navigation__item"><a href="#telas" className="navigation__link">Telas</a></li>
-                  <li onClick={() => { handleCloseMenu()}} className="navigation__item"><a href="#about" className="navigation__link">Sobre Mí</a></li>
-                  <li onClick={() => { handleCloseMenu()}} className="navigation__item"><a href="#buy" className="navigation__link">Cómo comprar</a></li>
-                  <li onClick={() => { handleCloseMenu()}} className="navigation__item"><a href="https://www.instagram.com/kimieartesanal/" className="navigation__link"  target="_blank" rel="noopener noreferrer">Instagram</a></li>
+          <nav className="menu__nav">
+              <ul className="menu__list">
+                  <li onClick={() => { handleCloseMenu()}} className="menu__item"><a href="#agendas" className="menu__link">Agendas</a></li>
+                  <li onClick={() => { handleCloseMenu()}} className="menu__item"><a href="#cuadernos" className="menu__link">Cuadernos</a></li>
+                  <li onClick={() => { handleCloseMenu()}} className="menu__item"><a href="#kakeibo" className="menu__link">Kakeibo</a></li>
+                  <li onClick={() => { handleCloseMenu()}} className="menu__item"><a href="#babys" className="menu__link">Babys</a></li>
+                  <li onClick={() => { handleCloseMenu()}} className="menu__item"><a href="#mascotas" className="menu__link">Mascotas</a></li>
+                  <li onClick={() => { handleCloseMenu()}} className="menu__item"><a href="#telas" className="menu__link">Telas</a></li>
+                  <li onClick={() => { handleCloseMenu()}} className="menu__item"><a href="#about" className="menu__link">Sobre Mí</a></li>
+                  <li onClick={() => { handleCloseMenu()}} className="menu__item"><a href="#buy" className="menu__link">Cómo comprar</a></li>
+                  <li onClick={() => { handleCloseMenu()}} className="menu__item"><a href="https://www.instagram.com/kimieartesanal/" className="menu__link"  target="_blank" rel="noopener noreferrer">Instagram</a></li>
               </ul>
           </nav>
       </div>
