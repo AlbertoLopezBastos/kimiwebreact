cf71610563f61fb48a2c569b091bac68df6433ac
Alberto <elbeto182@hotmail.com>
2021-03-14 00:45:46 -0300

    fixes on grids

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,8 @@ export default function About() {
       <h1 className="heading-1">Sobre Kimi</h1>
       <img className="about__logo" src={Images.logo} alt="kimi logo"></img>  
       <div className="about__grid pt-xl pb-xl">
-        <img className="about__photo" src="https://via.placeholder.com/500x600" alt="kimi"></img>
+        <img className="about__photo" src="https://via.placeholder.com/400x500" alt="kimi"></img>
         <div className="about__text">
-          <h2 className="heading-3 pb-md">Lorem, ipsum dolor sit amet consectetur.</h2>
           <p className="about__p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error incidunt quibusdam veritatis tempore fugit laudantium est rem accusamus, illum non?</p>
 
         </div>
